de423d542bc52d9a91b5e208e3850ca4d1a22e3a
ruabsarnul <absar.ru@yandex.com>
2023-01-10 21:38:40 +0600

    Successfully connected to db

--- nmongodb.js
+++ nmongodb.js
@@ -1,14 +1,10 @@
 const express = require("express");
 const book_connection = require("./mongodb/edudb/book_connection");
 const app = express();
+app.use(express.json());
 const port = 9000;
 
-app.post("/post", async(req, res)=> {
-    // const book_connection_ = await book_connection();
-    // const data = await book_connection_.insert(req.body);
-    console.log(req.body);
-    res.send(req.body);
-})
+
 
 
 app.get("/", (req, res)=> {
@@ -17,4 +13,11 @@ app.get("/", (req, res)=> {
 
 app.listen(port, ()=> {
     console.log(`The app is running on port number ${port}, visit http://localhost:${port}`);
+})
+
+
+app.post("/post", async (req, res) => {
+    const book_connection_ = await book_connection();
+    const data = await book_connection_.insert(req.body);
+    res.send(req.body);
 })
