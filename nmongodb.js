686ea414a6618fd774ec2171f4f32cf37078d288
ruabsarnul <absar.ru@yandex.com>
2023-01-10 21:14:39 +0600

    error

--- nmongodb.js
+++ nmongodb.js
@@ -1,21 +1,20 @@
 const express = require("express");
+const book_connection = require("./mongodb/edudb/book_connection");
 const app = express();
 const port = 9000;
-// const  privateKey = require("./mongodbPrivateKey");
-console.log(privateKey.projects.db_book);
+
+app.post("/post", async(req, res)=> {
+    // const book_connection_ = await book_connection();
+    // const data = await book_connection_.insert(req.body);
+    console.log(req.body);
+    res.send(req.body);
+})
+
 
 app.get("/", (req, res)=> {
     res.send("The app is running")
 })
 
-
-
-
-
-
-
-
-
-app.listen(port, (req, res)=> {
+app.listen(port, ()=> {
     console.log(`The app is running on port number ${port}, visit http://localhost:${port}`);
 })
